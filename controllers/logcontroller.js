2c888b3a1a953fe90d70abf74920920d1a724572
Kasey Schwartz <kcmjschwartz@gmail.com>
2021-05-28 12:12:50 -0400

    gets for all logs by user and specific log by user added

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const {LogModel} = require('../models');
 const middleware = require("../middleware");
 
-/**
+/*
 ===============
- * CREATE LOG
+* CREATE LOG
 ===============
 */
 
@@ -27,7 +27,68 @@ router.post('/create', middleware.validateSession, async (req, res) =>{
     }
 });
 
+/*
+====================
+* GET LOGS BY USER
+====================
+ */
 
+router.get("/mine", middleware.validateSession, async(req, res) => {
+    let {id} = req.user;
+    try{
+        const userLogs = await LogModel.findAll({
+            where:{
+                owner_id: id
+            }
+        });
+        res.status(200).json(userLogs);
+    } catch (err) {
+        res.status(500).json({ 
+            message: "Unable to retrieve logs",
+            error: err });
+    }
+});
+
+/*
+=======================
+* GET LOGS BY LOG ID
+=======================
+*/
+
+router.get("/mine/:entryId", middleware.validateSession, async(req, res) => {
+    const logId = req.params.entryId;
+    const userId = req.user.id;
+    try {
+    const results = await LogModel.findAll({
+        where: {
+            id: logId,
+            owner_id: userId
+        }
+    });
+        res.status(200).json(results);
+} catch (err) {
+    res.status(500).json({
+        message:'Unable to retrieve log',
+        error: err
+    })
+}    
+    
+});
+
+/*
+=======================
+* UPDATE LOGS 
+=======================
+*/
+
+
+
+
+/*
+=======================
+* DELETE LOGS 
+=======================
+*/
 
 
 module.exports = router;
