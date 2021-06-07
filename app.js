d7bb1db3ff477af567dd70d5f5b9b50bd09b26c5
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-07 17:52:37 -0400

    fixed headers

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const Express = require('express');
 const app = Express();
 const dbConnection = require('./db');
 
+
 const controllers = require('./controllers');
 app.use(Express.json());
+app.use(require('./middleware/headers'));
 app.use('/user', controllers.userController);
 app.use('/log', controllers.logController);
 
 dbConnection.authenticate()
     .then(() => dbConnection.sync())
     .then(() => {
-        app.listen(3000, () => {
-            console.log(`[Server]: App is listening on 3000.`);
+        app.listen(4000, () => {
+            console.log(`[Server]: App is listening on 4000.`);
         });
     })
     .catch ((err) => {
